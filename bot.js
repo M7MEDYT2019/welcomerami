3723dea837978b8ab8a9d42d7e9e2c4d30e03981
Mohamed <42390271+M7MEDYT2019@users.noreply.github.com>
2018-12-18 22:07:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,15 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("520292865092616203");
+const mohamed= member.guild.channels.get("523411510991519744");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(` **.. Welcome To, - __Future__ Community. :leaves:.:tulip:** `), 4000)        
+setTimeout(() => mohamed.send(` **' Welcome To Server Magic , منور يا قلب :heart: `), 4000)        
 }
 });
 
 client.on('message', msg => {
-  if (msg.content === '**.. Welcome To, - __Future__ Community. :leaves:.:tulip:**') {      
+  if (msg.content === '**' Welcome To Server Magic , منور يا قلب :heart:**') {      
     msg.channel.send(".")
   }
 });
